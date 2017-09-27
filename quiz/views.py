fa5fee3cb990343732d9696d301824ddd1ea3538
Lobsterass <Lobsterass@users.noreply.github.com>
2017-09-27 11:59:03 +0200

    Ändring i 4 views

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
 def startpage(request):
-	return render(request, "start.html")
+	return render(request, "startpage.html")
 
 def quiz(request, quiz_number):
+	return render(request, "quiz.html")
+
+def question(request, quiz_number, question_number):
+	return render(request, "question.html")
+
+def completed(request, quiz_number):
+	return rednder(request, "completed.html")
